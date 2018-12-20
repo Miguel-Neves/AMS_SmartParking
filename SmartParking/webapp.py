004649fe33e952b6e5a300c5699dc4a102a0cbb7
Miguel Neves <miguelneves@ua.pt>
2018-12-20 19:11:45 +0000

    ,

--- SmartParking/webapp.py
+++ SmartParking/webapp.py
@@ -218,7 +218,7 @@ if __name__ == '__main__':
         '/static': {
             'tools.staticdir.on': True,
             'tools.staticdir.dir': './static'
-        }
+        },
         'global': {
         	'server.socket_host': '0.0.0.0',
         	'server.socket_port': int(os.environ.get('PORT', 5000)),
